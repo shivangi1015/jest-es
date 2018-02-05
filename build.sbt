60ca787aa8100c3f79d62dbd9b71a9cb2f5ce10b
Shivangi Gupta <shivangi.gupta@knoldus.in>
2018-02-05 14:02:11 +0530

    Added CRUD operations to create, query, update and delete indexes from ElasticSearch

--- build.sbt
+++ build.sbt
@@ -7,8 +7,14 @@ scalaVersion := "2.12.4"
 val ES_VERSION = "5.3.3"
 val jestClient = "io.searchbox" % "jest" % ES_VERSION
 val playJson = "com.typesafe.play" %% "play-json" % "2.6.8"
+val es = "org.elasticsearch" % "elasticsearch" % ES_VERSION
+val log4jCore = "org.apache.logging.log4j" % "log4j-core" % "2.10.0"
+val log4jApi = "org.apache.logging.log4j" % "log4j-api" % "2.10.0"
 
 libraryDependencies ++= Seq(
   jestClient,
-  playJson
+  playJson,
+  es,
+  log4jCore,
+  log4jApi
 )
--- src/main/scala/com/knoldus/es/esoperations/ElasticSearchOperations.scala
+++ src/main/scala/com/knoldus/es/esoperations/ElasticSearchOperations.scala
@@ -1,10 +1,15 @@
 package com.knoldus.es.esoperations
 
+import java.lang
+
 import com.knoldus.es.client.ESClient
 import com.knoldus.es.request.ESDocument
 import io.searchbox.client.JestClient
+import io.searchbox.core._
+import org.elasticsearch.index.query.QueryBuilders.matchAllQuery
+import org.elasticsearch.index.query.{BoolQueryBuilder, MatchAllQueryBuilder, QueryBuilders}
+import org.elasticsearch.search.builder.SearchSourceBuilder
 import play.api.libs.json.Json
-import io.searchbox.core.{DocumentResult, Index, Search, SearchResult}
 
 
 trait ElasticSearchOperations {
@@ -16,16 +21,31 @@ trait ElasticSearchOperations {
 
     //creating index in ElasticSearch
     val index: Index = new Index.Builder(source)
-        .index(eSDocument.index)
-        .`type`(eSDocument.docType)
-        .id(eSDocument.docId)
-    .build()
-
+      .index(eSDocument.index)
+      .`type`(eSDocument.docType)
+      .id(eSDocument.docId)
+      .build()
     //executing client
     client.execute(index)
   }
 
+  def search(index: String, docType: String): lang.Long = {
+    //build query with QueryBuilder
+    val searchSourceBuilder: SearchSourceBuilder = new SearchSourceBuilder()
+    val query = searchSourceBuilder.query(QueryBuilders.matchAllQuery())
+    println("Query:: " + query.toString)
+    val search = new Search.Builder(query.toString).addIndex(index).addType(docType).build()
+    client.execute(search).getTotal
+  }
 
+  def update[T](eSDocument: ESDocument[T], index: String, docType: String)(implicit tjs: play.api.libs.json.Writes[T]) = {
+    val source = Json.stringify(Json.toJson(eSDocument.obj))
+    val updateQuery = new Update.Builder(source).index(index).`type`(docType).build()
+    client.execute(updateQuery)
+  }
 
-
+  def delete(index: String): DocumentResult = {
+    val deleteRequest = new Delete.Builder("").index(index).build()
+    client.execute(deleteRequest)
+  }
 }
